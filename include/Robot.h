191029ff1c91e3ba0ee9d2534ab7b888c5c49b41
Leman-Quinn <PabloN.A@hotmail.com>
2026-07-24 23:52:04 -0300

    inicio de clase Robot

--- include/Robot.h
+++ include/Robot.h
@@ -2,14 +2,25 @@
 #define ROBOT_H
 
 #include "UltrasonicSensor.h"
+#include "Driver.h"
 #include "Motor.h"
 
 class Robot {
     private:
+        UltrasonicSensor _leftUltrasonic;
+        UltrasonicSensor _frontLeftUltrasonic;
+        UltrasonicSensor _frontRightUltrasonic;
+        UltrasonicSensor _rightUltrasonic;
+        Driver _driver;
 
     public:
-        // CONSTRUCTOR
+        //---------- CONSTRUCTOR ----------//
         Robot();
+
+        //---------- METHODS ----------//
+        void begin();
+        void updateUltrasonic();
+        //void updateInfrared();
 };
 
 #endif
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,3 +2,21 @@
 
 #include <Arduino.h>
 
+ //---------- CONSTRUCTOR ----------//
+Robot::Robot(){
+
+}
+
+//---------- METHODS ----------//
+void Robot::begin(){
+    _leftUltrasonic.begin();
+    _frontLeftUltrasonic.begin();
+    _frontRightUltrasonic.begin();
+    _rightUltrasonic.begin();
+    _driver.begin();
+}
+
+void Robot::updateUltrasonic(){
+    // all in one or two different arrays??
+
+}
