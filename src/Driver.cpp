adf975bd7f17e87a6e33a10dab80d90b699d7067
Oli-Hanczyc <oli.hanczyc@gmail.com>
2026-08-29 16:59:33 -0300

    Desarrollo de métodos forwardRight() y forwardLeft(). Corrección en cálculo de pwm en el método backward(). Agregado del seteo de pwm para cada motor, con analogWrite, en métodos rotateRight() y rotateLeft().

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -25,11 +25,31 @@ void Driver::forward(int pwm_perc){
     analogWrite(_pwmb, pwm_raw);
 }
 
+void Driver::forwardRight(int pwm_perc){
+    _leftMotor.forward();
+    _rightMotor.forward();
+
+    int pwm_raw = (pwm_perc*255)/100; // pwm for the right motor
+
+    analogWrite(_pwma, 255); // the left motor has to have 255 pwm
+    analogWrite(_pwmb, pwm_raw); // right motor
+}
+
+void Driver::forwardLeft(int pwm_perc){
+    _leftMotor.forward();
+    _rightMotor.forward();
+
+    int pwm_raw = (pwm_perc*255)/100; // pwm for the left motor
+
+    analogWrite(_pwma, pwm_raw); // left motor
+    analogWrite(_pwmb, 255); // the right motor has to have 255 pwm
+}
+
 void Driver::backward(int pwm_perc){
     _leftMotor.backward();
     _rightMotor.backward();
 
-    int pwm_raw = (pwm_perc/255)/100;
+    int pwm_raw = (pwm_perc*255)/100;
 
     analogWrite(_pwma, pwm_raw);
     analogWrite(_pwmb, pwm_raw);
@@ -38,11 +58,17 @@ void Driver::backward(int pwm_perc){
 void Driver::rotateRight(){
     _leftMotor.forward();
     _rightMotor.backward();
+
+    analogWrite(_pwma, 255); // motor left
+    analogWrite(_pwmb, 255); // motor right (it needs 255 pwm to go backwards)
 }
 
 void Driver::rotateLeft(){
     _leftMotor.backward();
     _rightMotor.forward();
+
+    analogWrite(_pwma, 255); // motor left (it needs 255 pwm to go backwards)
+    analogWrite(_pwmb, 255); // motor right
 }
 
 void Driver::brake(){
